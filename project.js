7cff21a5e53eaa18c669bd79d1a28139703b554f
levg34 <guilhem.dross@laposte.net>
2017-06-14 00:49:44 +0200

    display all permutations of a word directly

--- project.js
+++ project.js
@@ -13,3 +13,27 @@ app.controller('testCtrl', function($scope) {
 		return res
 	}
 })
+
+app.controller('testCtrl2', function($scope) {
+	$scope.word = 'word'
+	$scope.permut = function(string) {
+		if (string.length < 2) return string; // This is our break condition
+
+		var permutations = []; // This array will hold our permutations
+
+		for (var i=0; i<string.length; i++) {
+			var char = string[i];
+
+			// Cause we don't want any duplicates:
+			if (string.indexOf(char) != i) // if char was used already
+				continue;           // skip it this time
+
+			var remainingString = string.slice(0,i) + string.slice(i+1,string.length); //Note: you can concat Strings via '+' in JS
+
+			for (var subPermutation of $scope.permut(remainingString))
+				permutations.push(char + subPermutation)
+
+		}
+		return permutations;
+	}
+})
